b98b6d2f4f7990918addb6c9e1207e0f38cd8d7f
Rai <agentydragon@gmail.com>
2022-06-10 23:06:59 -0700

    Add Manifold h-index

---
+++ manifold_h_index.py
@@ -0,0 +1,52 @@
+# http://manifold.markets/api/v0/markets?limit=1&before=id
+import requests
+import urllib
+import pandas as pd
+
+BASE_URI = 'http://manifold.markets/api/v0'
+
+before = None
+# limit: maximum and default is 1000
+limit = 1000
+
+markets = []
+
+while True:
+    params = {
+        'limit': limit,
+    }
+    if before:
+        params['before'] = before
+    url = f'{BASE_URI}/markets?' + urllib.parse.urlencode(params)
+    print(url)
+    result = requests.get(url)
+    j = result.json()
+    if len(j) < limit:
+        print("No more markets")
+        break
+    before = j[-1]['id']
+    markets.extend(j)
+
+recs = []
+for market in markets:
+    # https://manifold.markets/agentydragon/what-will-be-my-10-m-impact-factor
+    # at least N markets all of which have at least N bets of at least 10 M$
+    if market['creatorUsername'] == 'agentydragon':
+        # get bets
+        url = f'{BASE_URI}/market/{market["id"]}'
+        result = requests.get(url)
+        # bets, comments
+        num_bets = 0
+        for bet in result.json()['bets']:
+            if bet['amount'] >= 10:
+                num_bets += 1
+        recs.append({
+            'id': market['id'],
+            'num_bets': num_bets,
+        })
+
+df = pd.DataFrame(recs)
+df.sort_values('num_bets', ascending=False, inplace=True)
+df.reset_index(inplace=True, drop=True)
+print(df)
+# creatorName
