f964941d6a8380613dd3605ed2b44e4e7efb8aa9
Rai <agentydragon@gmail.com>
2025-04-11 18:02:25 -0700

    wip

--- dotfiles/config/nvim/lua/plugins/nvim-treesitter.lua
+++ dotfiles/config/nvim/lua/plugins/nvim-treesitter.lua
@@ -1,23 +1,80 @@
 local M = {
-    "nvim-treesitter/nvim-treesitter",
-    build = function()
-        require("nvim-treesitter.install").update({ with_sync = true })()
-    end,
-    event = { "BufReadPost", "BufNewFile" },
-    config = function()
-        require("nvim-treesitter.configs").setup({
-            -- List of parser names, or "all" (listed parsers MUST always be installed)
-            ensure_installed = {
-                "python", "c", "lua", "vim", "vimdoc", "query",
-                "markdown", "markdown_inline", "rust", "javascript",
-            },
-            -- Automatically install missing parsers when entering buffer
-            -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-            auto_install = true,
-            highlight = { enable = true },
-        })
-    end,
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	--build = function()
+	--	require("nvim-treesitter.install").update({ with_sync = true })()
+	--end,
+	event = { "BufReadPost", "BufNewFile" },
+	config = function()
+		require("nvim-treesitter.configs").setup({
+			-- List of parser name to always have installed, or "all"
+			ensure_installed = {
+				"bash",
+				"bibtex",
+				"c",
+				"c_sharp",
+				"clojure",
+				"cmake",
+				"cpp",
+				"css",
+				"csv",
+				"desktop",
+				"diff",
+				"dockerfile",
+				"git_config",
+				"git_rebase",
+				"gitattributes",
+				"gitcommit",
+				"gitignore",
+				"go",
+				"gomod",
+				"gosum",
+				"gotmpl",
+				"haskell",
+				"html",
+				"htmldjango",
+				"http",
+				"ini",
+				"java",
+				"javadoc",
+				"javascript",
+				"jinja",
+				"jq",
+				"jsdoc",
+				"json",
+				"jsonnet",
+				"latex",
+				"lua",
+				"luadoc",
+				"make",
+				"markdown",
+				"nginx",
+				"nix",
+				"proto",
+				"python",
+				"requirements",
+				"rust",
+				"scss",
+				"sql",
+				"ssh_config",
+				"starlark",
+				"textproto",
+				"tmux",
+				"toml",
+				"typescript",
+				"vim",
+				"vimdoc",
+				"xml",
+			},
+			-- Install parsers asynchronously (only applied to `ensure_installed`)
+			sync_install = false,
+			-- Automatically install missing parsers when entering buffer
+			-- Recommendation: set to false if you don't have `tree-sitter`
+			-- CLI installed locally
+			auto_install = true,
+			highlight = { enable = true },
+			indent = { enable = true },
+		})
+	end,
 }
 return { M }
-
-
