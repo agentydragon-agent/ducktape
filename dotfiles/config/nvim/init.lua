1155e32ea06b0456c3d9b8c09f5345e61bd7a957
Rai <agentydragon@gmail.com>
2025-04-12 18:50:42 -0700

    wip

--- dotfiles/config/nvim/init.lua
+++ dotfiles/config/nvim/init.lua
@@ -1,128 +1,91 @@
---set tabstop=8
---set shiftwidth=8
---set softtabstop=8
+--[[
+set tabstop=8
+set shiftwidth=8
+set softtabstop=8
+
+-- fold by syntax, open all by default
+set foldmethod=syntax
+set foldlevelstart=20
+
+set omnifunc=syntaxcomplete#Complete
+
+" no mistyped :w or :q...
+:command W w
+:command Q q
+
+set laststatus=2
+
+Not yet ported plugins I had listed in old vim config:
+  wincent/command-t, tpope/vim-fugitive, pbrisbin/vim-mkdir' " mkdir needed dirs before writing buffer
+  bazelbuild/vim-ft-bzl, google/vim-maktaba (vim-codefmt dep), vim-codefmt, vim-glaive (configures codefmt's maktaba flags),
+  leafgarland/typescript-vim, wellle/context.vim
+
+ set textwidth=80
+
+autocmd Filetype c setlocal cindent nosmartindent
+" TODO: enable this settings
+" autocmd Filetype cpp setlocal cindent nosmartindent tabstop=2 softtabstop=2 expandtab
+
+let g:netrw_banner=0  " Hide netrw banner
+
+let g:matchparen_insert_timeout=5
+
+set colorcolumn=80,+0  " Highlight column 80
+
+-- Set codefmt autoformatter settings
+augroup autoformat_settings
+  autocmd FileType bzl AutoFormatBuffer buildifier
+  autocmd FileType c,cpp,proto,javascript AutoFormatBuffer clang-format
+  autocmd FileType go AutoFormatBuffer gofmt
+  autocmd FileType rust AutoFormatBuffer rustfmt
+  " autocmd FileType html,css,json AutoFormatBuffer js-beautify
+  autocmd FileType java AutoFormatBuffer google-java-format
+augroup END
+
+set foldmethod=syntax
+set foldcolumn=1
+let javaScript_fold=1 "activate folding by JS syntax
+set foldlevelstart=99
+
+" Map Alt-T to paste current datetime
+inoremap <A-t> <C-R>=strftime('%Y-%m-%d %H:%M:%S')<C-M>
+]]
 --
---set smartindent
---
---set encoding=utf-8
---set wildmenu
---set hidden
---
---set ttyfast
---
---" fold by syntax, open all by default
---set foldmethod=syntax
---set foldlevelstart=20
---
---" chci aspon 5 radku mezi koncem stranky a kurzorem
---set scrolloff=5
---
---set omnifunc=syntaxcomplete#Complete
---
---" to byvalo <S-M>, ale to koliduje s 'vyber prostredni radku'
---noremap <Leader>m :make<CR>
---
---" no mistyped :w or :q...
---:command W w
---:command Q q
---
---set showcmd
---set laststatus=2
---
---" Plug 'wincent/command-t'
---" Plug 'tpope/vim-fugitive'
---Plug 'derekwyatt/vim-scala'
---Plug 'pbrisbin/vim-mkdir' " mkdir needed dirs before writing buffer
---Plug 'bazelbuild/vim-ft-bzl'
---Plug 'google/vim-maktaba'  " dependency of vim-codefmt
---Plug 'google/vim-codefmt'
---Plug 'google/vim-glaive'  " used to configure codefmt's maktaba flags
---Plug 'leafgarland/typescript-vim'
---Plug 'cespare/vim-toml'
---Plug 'wellle/context.vim'
---
---set textwidth=80
---
---" This is taken from: https://github.com/cespare/vim-toml/blob/master/ftdetect/toml.vim
---" TODO: Why is this necessary?
---autocmd BufNewFile,BufRead *.toml,Gopkg.lock,Cargo.lock,*/.cargo/config,*/.cargo/credentials,Pipfile setf toml
---
---autocmd Filetype c setlocal cindent nosmartindent
---" TODO: enable this settings
---" autocmd Filetype cpp setlocal cindent nosmartindent tabstop=2 softtabstop=2 expandtab
---
---" Hide stuff in:
---"   anything from .gitignore,
---"   the . and .. entries,
---"   Vim swapfiles,
---"   .git directory
---"   unuseful stuff in ~
---let g:netrw_list_hide= join([
---\ netrw_gitignore#Hide(),
---\  '.*\.sw?$',
---\  '^\./$',
---\  '^\.\./$',
---\  '.*/\.git/$'],
---\',')
---" Hide netrw banner
---let g:netrw_banner=0
---
---let g:matchparen_insert_timeout=5
---
---" Highlight end of line whitespace and mixed spaces and tabs
---highlight ExtraWhitespace ctermbg=darkgreen guibg=darkgreen
---autocmd ColorScheme * highlight ExtraWhitespace ctermbg=red guibg=red
---match ExtraWhitespace /\s\+$\| \+\ze\t/
---au InsertEnter * match ExtraWhitespace /\s\+\%#\@<!$\| \+\ze\t/
---au InsertLeave * match ExtraWhitespace /\s\+$\| \+\ze\t/
---
---" u mad bro?
---inoremap <Left> ←
---inoremap <Right> →
---inoremap <Up> ↑
---inoremap <Down> ↓
---inoremap <S-Left> ⇐
---inoremap <S-Right> ⇒
---inoremap <S-Up> ⇑
---inoremap <S-Down> ⇓
---
---" Highlight column 80
---set colorcolumn=80,+0
---
---" set t_Co=256
---
---" Jump to last position on opening files (stolen from Destroy All Software)
---" ('\" == mark when last exiting buffer, g` = go to, jumplist-nondestructive)
---autocmd BufReadPost * if line("'\"") > 0 && line("'\"") <= line("$") | exe "normal g`\"" | endif
---
---" Don't clear screen after exiting Vim.
---" http://www.shallowsky.com/linux/noaltscreen.html
---" set t_ti= t_te=
---
---" Set codefmt autoformatter settings
---augroup autoformat_settings
---  autocmd FileType bzl AutoFormatBuffer buildifier
---  autocmd FileType c,cpp,proto,javascript AutoFormatBuffer clang-format
---  autocmd FileType go AutoFormatBuffer gofmt
---  autocmd FileType rust AutoFormatBuffer rustfmt
---  " autocmd FileType html,css,json AutoFormatBuffer js-beautify
---  autocmd FileType java AutoFormatBuffer google-java-format
---augroup END
---
---set foldmethod=syntax
---set foldcolumn=1
---let javaScript_fold=1 "activate folding by JS syntax
---set foldlevelstart=99
---
---" Map Alt-T to paste current datetime
---inoremap <A-t> <C-R>=strftime('%Y-%m-%d %H:%M:%S')<C-M>
---
---" Copilot
---let g:copilot_filetypes = {
---\ 'yaml': v:true,
---\ 'markdown': v:true,
---\ }
+
+-- Hide useless stuff in netrw
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = "netrw",
+	callback = function()
+		vim.g.netrw_list_hide = table.concat({
+			vim.fn["netrw_gitignore#Hide"](), -- auto hide based on .gitignore
+			".*%.sw[op]$", -- Vim swap
+			"^\\./$", -- current dir entry
+			-- '^\\.\\./$', -- parent dir entry (TODO: use - key)
+			".*/%.git/$", -- .git dirs
+		}, ",")
+	end,
+})
+
+-- TODO: give warning or highlight if the same file has a mix of lines starting with spaces and tabs
+-- (rather that just one of the two options)
+
+-- u mad bro?
+local i = vim.keymap.set
+for lhs, rhs in pairs({
+	["<Left>"] = "←",
+	["<Right>"] = "→",
+	["<Up>"] = "↑",
+	["<Down>"] = "↓",
+	["<S-Left>"] = "⇐",
+	["<S-Right>"] = "⇒",
+	["<S-Up>"] = "⇑",
+	["<S-Down>"] = "⇓",
+}) do
+	i("i", lhs, rhs)
+end
+
 local opt = vim.opt
+opt.scrolloff = 5 -- at least 5 lines between cursor and end of page
 opt.number = true -- Show line numbers
 opt.hlsearch = true -- Highlight search matches
 opt.incsearch = true -- Show search matches as you type
@@ -144,30 +107,9 @@ opt.termguicolors = true -- Enable true color support (recommended for modern UI
 -- opt.backup = false              -- Don't create backup files
 
 -- maybe -- try out:
--- opt.relativenumber = true       -- Relative line numbers
+-- opt.relativenumber = true       -- Show relative line numbers outside current line
 -- opt.splitright = true           -- Split vertical windows to the right
 -- opt.splitbelow = true           -- Split horizontal windows to the bottom
-
--- Set up color scheme updating
--- Set color theme to light/dark based on current system preferences.
--- Done early to prefer flashing of the wrong theme before this runs.
--- Will later be picked up when setting up Solarized colors.
--- Called on theme switches by set_light_theme, set_dark_theme scripts.
----_G.UpdateThemeFromGnome = function()
----	-- Run gsettings
----	local color_scheme = vim.fn.system("gsettings get org.gnome.desktop.interface color-scheme")
----	-- strip newline
----	color_scheme = color_scheme:gsub("\n", "")
----	-- remove quotes
----	color_scheme = color_scheme:gsub("'", "")
----
----	if color_scheme == "prefer-dark" then
----		vim.o.background = "dark"
----	else
----		vim.o.background = "light"
----	end
----end
----_G.UpdateThemeFromGnome()
 
 -- Make sure to setup `mapleader` and `maplocalleader` before
 -- loading lazy.nvim so that mappings are correct.
@@ -176,14 +118,6 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
 require("config.lazy")
-
--- NeoSolarized: status line not rendered with any usable color:
--- {
---   "Tsuzat/NeoSolarized.nvim",
---   lazy = false, -- make sure we load this during startup if it is your main colorscheme
---   priority = 1000, -- make sure to load this before all the other start plugins
---   config = function() vim.cmd([[ colorscheme NeoSolarized ]]) end,
--- }
 
 -- Django template formatter:
 -- { "yaegassy/coc-htmldjango" }
--- dotfiles/config/nvim/lua/plugins/copilot.lua
+++ dotfiles/config/nvim/lua/plugins/copilot.lua
@@ -1,2 +1,10 @@
---- https://github.com/zbirenbaum/copilot-cmp maybe ?
-return { "github/copilot.vim" }
+--- https://github.com/zbirenbaum/copilot-cmp maybe ? (turns copilot into cmp source)
+return {
+	"github/copilot.vim",
+	init = function()
+		vim.g.copilot_filetypes = {
+			yaml = true,
+			markdown = true,
+		}
+	end,
+}
