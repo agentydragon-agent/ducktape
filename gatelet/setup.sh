d1c353e63f24e2f9eaaf997f600632301ff15b10
Rai <agentydragon@gmail.com>
2025-05-21 01:38:47 -0700

    wip

--- gatelet/setup.sh
+++ gatelet/setup.sh
@@ -29,33 +29,33 @@ echo "export IS_CODEX_ENV=1" >> /root/.bashrc
 
 python_env_setup() {
     pip install --upgrade pip wheel
-    #pip install -e gatelet[dev]
-    pip install \
-    "pytest==7.3.1" \
-    "pytest-asyncio==0.21.0" \
-    "pytest-timeout==2.2.0" \
-    "isort==5.12.0" \
-    "pyhamcrest==2.0.4" \
-    "httpx==0.24.1" \
-    "pytest-postgresql==4.1.1" \
-    "sqlalchemy-stubs==0.4.0" \
-    "fastapi>=0.104.0" \
-    "uvicorn[standard]>=0.23.2" \
-    "SQLAlchemy>=2.0.0" \
-    "alembic>=1.12.0" \
-    "psycopg2-binary>=2.9.6" \
-    "asyncpg>=0.27.0 "\
-    "cryptography>=41.0.0" \
-    "jinja2>=3.1.2" \
-    "python-multipart>=0.0.9" \
-    "pydantic>=2.4.0" \
-    "compact_json>=1.0.0" \
-    "homeassistant-api" \
-    "passlib" \
-    "tomlkit" \
-    "fastapi-csrf-protect" \
-    "fastapi-users[sqlalchemy,argon2]" \
-    "fastapi-sessions"
+    pip install -e gatelet[dev]
+    # pip install \
+    # "pytest==7.3.1" \
+    # "pytest-asyncio==0.21.0" \
+    # "pytest-timeout==2.2.0" \
+    # "isort==5.12.0" \
+    # "pyhamcrest==2.0.4" \
+    # "httpx==0.24.1" \
+    # "pytest-postgresql==4.1.1" \
+    # "sqlalchemy-stubs==0.4.0" \
+    # "fastapi>=0.104.0" \
+    # "uvicorn[standard]>=0.23.2" \
+    # "SQLAlchemy>=2.0.0" \
+    # "alembic>=1.12.0" \
+    # "psycopg2-binary>=2.9.6" \
+    # "asyncpg>=0.27.0 "\
+    # "cryptography>=41.0.0" \
+    # "jinja2>=3.1.2" \
+    # "python-multipart>=0.0.9" \
+    # "pydantic>=2.4.0" \
+    # "compact_json>=1.0.0" \
+    # "homeassistant-api" \
+    # "passlib" \
+    # "tomlkit" \
+    # "fastapi-csrf-protect" \
+    # "fastapi-users[sqlalchemy,argon2]" \
+    # "fastapi-sessions"
 }
 
 # pip install --upgrade pip setuptools wheel
