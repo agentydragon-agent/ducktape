e236e10a1c13de50c9775a2c35c2da97e2346458
agentydragon <agentydragon@users.noreply.github.com>
2026-01-12 21:25:49 -0800

    Nest user SSH and group settings under single block

--- nix/nixos/hosts/wyrm2/default.nix
+++ nix/nixos/hosts/wyrm2/default.nix
@@ -18,12 +18,13 @@
 
   # SSH authorized keys - will be injected by cloud-init initially
   # After first boot, manage via this config
-  users.users.${username}.openssh.authorizedKeys.keys = [
-    # Add your SSH public key here after initial provisioning
-    # "ssh-ed25519 AAAA... user@host"
-  ];
-
-  # Allow user to read system logs without sudo
-  users.users.${username}.extraGroups = ["systemd-journal"];
+  users.users.${username} = {
+    openssh.authorizedKeys.keys = [
+      # Add your SSH public key here after initial provisioning
+      # "ssh-ed25519 AAAA... user@host"
+    ];
+    # Allow user to read system logs without sudo
+    extraGroups = ["systemd-journal"];
+  };
   boot.kernel.sysctl."kernel.dmesg_restrict" = 0;
 }
