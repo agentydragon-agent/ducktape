e691fab7a6801b3074509524aeaf9c94532bac3d
Rai <agentydragon@gmail.com>
2026-03-10 02:53:45 -0700

    style: nixfmt gui.nix timekpr config

--- nix/nixos/modules/gui.nix
+++ nix/nixos/modules/gui.nix
@@ -29,17 +29,44 @@
       allowedHours = [
         # 22:00-01:00: allow :00-:50 (forced 10-min break before each hour)
         {
-          hours = [ 22 23 0 ];
+          hours = [
+            22
+            23
+            0
+          ];
           minuteRange = "00-50";
         }
         # 01:00-05:00: allow :00-:15 (mostly locked out overnight)
         {
-          hours = [ 1 2 3 4 ];
+          hours = [
+            1
+            2
+            3
+            4
+          ];
           minuteRange = "00-15";
         }
         # 05:00-22:00: full access
         {
-          hours = [ 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 ];
+          hours = [
+            5
+            6
+            7
+            8
+            9
+            10
+            11
+            12
+            13
+            14
+            15
+            16
+            17
+            18
+            19
+            20
+            21
+          ];
           minuteRange = "00-60";
         }
       ];
