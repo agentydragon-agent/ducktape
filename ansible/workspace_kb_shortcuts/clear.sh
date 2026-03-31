e4d5771b03a1b65696a009ba7c3b528ab8b5f78a
Rai <agentydragon@gmail.com>
2026-03-30 22:00:36 -0700

    ansible: Delete old workspace kb shortcut investigation

--- ansible/workspace_kb_shortcuts/clear.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# 2. clear Pop-Shell workspace grabs
-schema=org.gnome.shell.extensions.pop-shell
-for k in switch-to-workspace-{left,right} move-to-workspace-{left,right}; do
-  gsettings set "$schema" "$k" "@as []"
-done
