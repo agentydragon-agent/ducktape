36271d26a395cbc14bc8b8da4baf8002507d5fc4
Rai <agentydragon@gmail.com>
2026-04-24 22:06:48 -0700

    Wire gaffer-private fetch token into nix-daemon

--- nix/nixos/hosts/wyrm2/default.nix
+++ nix/nixos/hosts/wyrm2/default.nix
@@ -40,6 +40,7 @@ in
     ../../modules/gui.nix
     ../../modules/workstation.nix
     ../../modules/bazel
+    ../../modules/github-fetch-token.nix
     ../../modules/system-inspection-sudo.nix
     ../../modules/k8s-worker.nix
     ../../modules/gpu-monitor.nix
@@ -47,6 +48,10 @@ in
 
   # Passwordless sudo for system inspection commands
   ducktape.systemInspectionSudo.enable = true;
+  ducktape.githubFetchToken = {
+    enable = true;
+    sopsFile = ../../../../secrets/shared/gaffer-private-fetch-pat.yaml;
+  };
 
   ducktape.k8sWorker = {
     enable = true;
